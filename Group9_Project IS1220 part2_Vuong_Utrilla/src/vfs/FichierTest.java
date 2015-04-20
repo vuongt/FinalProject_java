a851c62a763e985025e3c8f3dce587b30d5a1ebe
vtat <thi.vuong@student.ecp.fr>
2015-04-20 18:38:02 +0200

    FicherTest

---
+++ Group9_Project IS1220 part2_Vuong_Utrilla/src/vfs/FichierTest.java
@@ -0,0 +1,22 @@
+package vfs;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FichierTest {
+
+	@Test
+	public void testGetAbsolutePath() throws DuplicatedNameException, InvalidInput {
+		Fichier file11 = new Fichier("file11");
+		VFS vfs1 = new VFS();
+		VirtualDisk Root = new VirtualDisk("Root",1000);
+		vfs1.getVirtualDisks().put("Root", Root);
+		Directory D1 = new Directory("D1");
+		Root.addDirectory(D1);
+		D1.addFile(file11);
+		assertEquals(file11.getAbsolutePath(),"/D1/file11");
+		}
+
+}
